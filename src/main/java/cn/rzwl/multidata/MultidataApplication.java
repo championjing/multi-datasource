4d1b3840dafd112465cd4e1e77d04e40159faf86
Champoin <championjing@163.com>
2019-07-09 22:52:34 +0800

    initialize

---
+++ src/main/java/cn/rzwl/multidata/MultidataApplication.java
@@ -0,0 +1,13 @@
+package cn.rzwl.multidata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultidataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultidataApplication.class, args);
+    }
+
+}
